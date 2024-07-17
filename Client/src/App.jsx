4a890c3c026e7d8ebb0a1a0befbab80eaa9dd988
Manesha-99 <maneshatharindaofficial@gmail.com>
2024-07-17 16:01:50 +0530

    Add Routes ande created pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 function App() {
-  
-
   return (
-    <>
-      <h1 className="bg-slate-500">Hello</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
