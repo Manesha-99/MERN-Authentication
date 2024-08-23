8960144622b098bf1f897e7d163dc1926bb5f082
Manesha-99 <maneshatharindaofficial@gmail.com>
2024-08-23 12:33:39 +0530

    Made profile private

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const Header = () => {
+  const {currentUser} = useSelector(state=> state.user);
   return (
-    <div className="bg-slate-400 p-4 flex justify-between items-center ">
+    <div className="flex items-center justify-between p-4 bg-slate-400 ">
       <Link to="/">
-        <h1 className="font-bold text-4xl ml-8">Auth App</h1>
+        <h1 className="ml-8 text-4xl font-bold">Auth App</h1>
       </Link>
       <div className="flex gap-6 text-2xl">
         <Link to="/">
@@ -14,8 +16,13 @@ const Header = () => {
         <Link to="about">
           <ul>About</ul>
         </Link>
-        <Link to="sign-in">
+        <Link to="profile">
+        {currentUser? (
+          <img src={currentUser.profilePicture} alt="profile" className="object-cover w-8 h-8 rounded-full"/>
+        ): (
           <ul className="mr-8">Sign-In</ul>
+        )}
+          
         </Link>
       </div>
     </div>
